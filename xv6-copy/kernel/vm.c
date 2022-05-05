49830f46ec33936ecf8d86f607fbd381c90f5f8f
YiminXia <1275499353@qq.com>
2022-05-06 00:06:32 +0800

    Lecture4-Page Tables 第10次提交

--- xv6-copy/kernel/vm.c
+++ xv6-copy/kernel/vm.c
@@ -216,6 +216,141 @@ uvminit(pagetable_t pagetable, uchar *src, uint sz)
     memmove(mem, src, sz);
 }
 
+// Allocate PTEs and physical memory to grow process from oldsz to newsz, 
+// which need not be page aligned. Returns new size or 0 on error.
+uint64
+uvmalloc(pagetable_t pagetable, uint64 oldsz, uint64 newsz)
+{
+    char    *mem;
+    uint64  a;
+
+    if(newsz < oldsz)
+        return oldsz;
+
+    oldsz = PGROUNDUP(oldsz);
+    for(a = oldsz; a < newsz; a += PGSIZE) {
+        mem = kalloc();
+        if(mem == 0) {
+            uvmdealloc(pagetable, a, oldsz);
+            return 0;
+        }
+        memset(mem, 0, PGSIZE);
+        if(mappages(pagetable, a, PGSIZE, (uint64)mem, PTE_W|PTE_X|PTE_R|PTE_U) != 0) {
+            kfree(mem);
+            uvmdealloc(pagetable, a, oldsz);
+            return 0;
+        }
+    }
+    return newsz;
+}
+
+// Deallocate user pages to bring the process size from oldsz to newsz.
+// oldsz and newsz need not be page-aligned, nor does newsz need to be less than oldsz.
+// oldsz can be larger than the actual process size.
+// Return the new process size. 将process size从oldsz降到newsz
+uint64
+uvmdealloc(pagetable_t pagetable, uint64 oldsz, uint64 newsz)
+{
+    if(newsz >= oldsz)
+        return oldsz;
+
+    if(PGROUNDUP(newsz) < PGROUNDUP(oldsz)) {
+        int npages = (PGROUNDUP(oldsz) - PGROUNDUP(newsz)) / PGSIZE;
+        uvmunmap(pagetable, PGROUNDUP(newsz), npages, 1);
+    }
+
+    return newsz;
+}
+
+// Recursively free page-table pages.
+// All leaf mapping must already have been removed. 只释放page-table的页，leaf不释放
+void
+freewalk(pagetable_t pagetable)
+{
+    // there are 2^9 = 512 PTEs in a page table.
+    for(int i = 0; i < 512; i++) {
+        pte_t pte = pagetable[i];
+        if((pte & PTE_V) && (pte & (PTE_R|PTE_W|PTE_X)) == 0) {
+            // this PTE points to a lower-level page table.
+            uint64 child = PTE2PA(pte);
+            freewalk((pagetable_t)child);
+            pagetable[i] = 0;
+        } else if(pte & PTE_V) {
+            panic("freewalk: leaf");
+        }
+    }
+    kfree((void *)pagetable);
+}
+
+// Free user memory pages.
+// then free page-table pages
+void
+uvmfree(pagetable_t pagetable, uint64 sz)
+{
+    if(sz > 0)
+        uvmunmap(pagetable, 0, PGROUNDUP(sz)/PGSIZE, 1);
+    freewalk(pagetable);
+}
+
+// Given a parent process's page table, copy its memory into a child's page table.
+// Copies both the page table and the physical memory.
+// Returns 0 on success, -1 on failure.
+// Free any allocated pages on failure.
+
+int
+uvmcopy(pagetable_t old, pagetable_t new, uint64 sz)
+{
+    pte_t   *pte;
+    uint64  pa, i;
+    uint    flags;
+    char    *mem;
+
+    for(i = 0; i < sz; i += PGSIZE) {
+        if((pte = walk(old, i, 0)) == 0)
+            panic("uvmcopy: pte should exist");
+        if((*pte & PTE_V) == 0)
+            panic("uvmcopy: page not present");
+        pa = PTE2PA(*pte);
+        flags = PTE_FLAGS(*pte);
+        if((mem = kalloc()) == 0)
+            goto err;
+        memmove(mem, (char *)pa, PGSIZE);
+        if(mappages(new, i, PGSIZE, (uint64)mem, flags) != 0) {
+            kfree(mem);
+            goto err;
+        }
+    }
+    return 0;
+
+err:
+    uvmunmap(new, 0, i/PGSIZE, 1);
+    return -1;
+}
+
+// mark a PTE invalid for user access.
+// used by exec for the user stack guard page.
+void
+uvmclear(pagetable_t pagetable, uint64 va)
+{
+    pte_t *pte;
+
+    pte = walk(pagetable, va, 0);
+    if(pte == 0)
+        panic("uvmclear");
+    *pte &= ~PTE_U; // *pte = *pte & ~PTE_V;
+}
+
+// Copy from kernel to user.
+// Copy len bytes from src to virtual address dstva in a given page table.
+// Return 0 on success, -1 on error.
+int
+copyout(pagetable_t pagetable, uint64 dstva, char *src, uint64 len)
+{
+
+}
+
+
+
 // Copy from user to kernel.
 // Copy len bytes to dst from virtual address srcva in a given page table.
 // Return 0 on success, -1 on error.
@@ -223,6 +358,16 @@ int
 copyin(pagetable_t pagetable, char *dst, uint64 srcva, uint64 len)
 {
     
+
+}
+
+// Copy a null-terminated string from user to kernel.
+// Copy bytes to dst from virtual address srcva in a given page table.
+// until a '\0', or max.
+// Return 0 on success, -1 on error.
+int
+copyinstr(pagetable_t pagetable, char *dst, uint64 srcva, uint64 max)
+{
 
 }
 
